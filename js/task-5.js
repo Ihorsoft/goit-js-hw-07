f9801ea3ef1c71f2bd6273b45147393ec898827e
Ihorsoft <0639492002s@gmail.com>
2024-04-27 16:27:26 +0300

    + task #5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,18 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const bodyStyle = document.querySelector("body");
+const buttonChange = document.querySelector(".change-color");
+const colorText = document.querySelector(".color");
+
+buttonChange.addEventListener("click", bodyStyleChange);
+
+function bodyStyleChange(event) { 
+  const backcolorNew = getRandomHexColor();
+  bodyStyle.style.backgroundColor = backcolorNew;
+  colorText.textContent = backcolorNew;
+
+   // return;
+};
